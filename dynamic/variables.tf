8c60c22b342ea5b64081ded432ffe6bc26fee76b
Renato Germani Brugnoni <renato.germani@dxc.com>
2023-09-13 14:55:12 +0300

    chore: add source code of dynamic values for security group

---
+++ dynamic/variables.tf
@@ -0,0 +1,27 @@
+variable "provider_region" {
+  description = "region for cloud provider"
+  type        = string
+}
+
+variable "ami" {
+  description = "ami id for vm"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "type of virtual machine"
+  type        = string
+}
+
+variable "sg_name" {
+  description = "name for security group"
+  type        = string
+}
+
+variable "ingressrules" {
+  type = list(number)
+}
+
+variable "egressrules" {
+  type = list(number)
+}
